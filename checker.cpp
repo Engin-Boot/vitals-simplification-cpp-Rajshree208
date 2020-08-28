70b51bbeb75b09d965c6e9a131373ab653be36e0
Rajshree208 <46247383+Rajshree208@users.noreply.github.com>
2020-08-28 12:07:56 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,22 +1,33 @@
 #include <assert.h>
 #include <climits>
 
-const int bpmlimits[] = {70,150};
+const int bpmlimits[] = { 70,150 };
 const int spo2min = 90;
-const int resplimits[]= {30,95};
+const int resplimits[] = { 30,95 };
 
-bool vitalsIsOk(float value, int lower,int upper){
-    return (value>=lower && value<=upper);
+bool vitalsIsOk(float value, int lower, int upper) {
+	return (value >= lower && value <= upper);
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (vitalsIsOk(bpm, bpmlimits[0],bpmlimits[1])&&vitalsIsOk(spo2,spo2min,INT_MAX)&&vitalsIsOk(respRate, resplimits[0],resplimits[1]));
+	int countOfTrueValues = 0;
+	const int total_no_of_parameters = 3;
+	int vitalsStatus[total_no_of_parameters] = { vitalsIsOk(bpm, bpmlimits[0],bpmlimits[1]),
+												vitalsIsOk(spo2,spo2min,INT_MAX),
+												vitalsIsOk(respRate, resplimits[0],resplimits[1]) };
+
+	for (int i = 0; i < total_no_of_parameters; i++) {
+		if (vitalsStatus[i] == 1) {
+			countOfTrueValues++;
+		}
+	}
+	return (countOfTrueValues == total_no_of_parameters);
 }
 
 int main() {
-  assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);
-  assert(vitalsIsOk(60, 70, 150) == false);
-  assert(vitalsIsOk(200, 90, 150) == false);
-  assert(vitalsIsOk(85, 70, 150) == true);
+	assert(vitalsAreOk(80, 95, 60) == true);
+	assert(vitalsAreOk(60, 90, 40) == false);
+	assert(vitalsIsOk(60, 70, 150) == false);
+	assert(vitalsIsOk(200, 90, 150) == false);
+	assert(vitalsIsOk(85, 70, 150) == true);
 }
