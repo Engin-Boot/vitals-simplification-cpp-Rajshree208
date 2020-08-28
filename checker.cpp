f1be9a0dd2244fcb6b6d49d859dbda4ee4bd7c84
Rajshree208 <46247383+Rajshree208@users.noreply.github.com>
2020-08-28 16:20:17 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -2,45 +2,82 @@
 #include <climits>
 #include <string>
 #include <iostream>
+using namespace std;
 
 const int bpmlimits[] = { 70,150 };
 const int spo2min = 90;
 const int resplimits[] = { 30,95 };
 const std::string vitalNames[] = { "BPM", "SPO2", "RESPIRATORY" };
 
-bool vitalsIsOk(std::string vitalName,float value, int lower, int upper) {
-    if(value<lower)
-    {
-		std::cout << vitalName << "-->" << value << " is lower than the expected lower value -->" << lower << std::endl;
-		return false;
-    }
-	if(value>upper)
+class Alert
+{
+public:
+	virtual void sendAlert(const string, const string) = 0;
+};
+
+class AlertWithSms :public Alert 
+{
+public:
+	void sendAlert(string vitalName, string message) override
 	{
-		std::cout << vitalName << "-->" << value << " is higher than the expected upper value -->" << upper << std::endl;
-		return false;
+		cout << "Alert using SMS" << "-->" << vitalName << " " << message << endl;
 	}
-	return true;
-}
+};
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-	int countOfTrueValues = 0;
-	const int total_no_of_parameters = 3;
-	int vitalsStatus[total_no_of_parameters] = { vitalsIsOk(vitalNames[0],bpm, bpmlimits[0],bpmlimits[1]),
-						     vitalsIsOk(vitalNames[1],spo2,spo2min,INT_MAX),
-						     vitalsIsOk(vitalNames[2],respRate, resplimits[0],resplimits[1]) };
+class AlertWithAlarm : public Alert
+{
+public:
+	void sendAlert(string vitalName, string message) override
+	{
+		cout << "Alert using Alarm" << "-->" << vitalName << " " << message << endl;
+	}
+};
 
-	for (int i = 0; i < total_no_of_parameters; i++) {
-		if (vitalsStatus[i] == 1) {
-			countOfTrueValues++;
+class VitalsCheck
+{
+public:
+
+	bool vitalsIsOk(Alert* alert, string vitalName, float value, int lower, int upper) {
+
+		if (value < lower)
+		{
+			//std::cout << vitalName << "-->" << value << " is lower than the expected lower value -->" << lower << std::endl;
+			string message = "IS LOW !!";
+			alert->sendAlert(vitalName, message);
+			return false;
 		}
+		if (value > upper)
+		{
+			//std::cout << vitalName << "-->" << value << " is higher than the expected upper value -->" << upper << std::endl;
+			string message = "IS HIGH !!";
+			alert->sendAlert(vitalName, message);
+			return false;
+		}
+		return true;
 	}
-	return (countOfTrueValues == total_no_of_parameters);
-}
+
+	bool vitalsAreOk(Alert* alert, float bpm, float spo2, float respRate) {
+		int countOfTrueValues = 0;
+		const int total_no_of_parameters = 3;
+		int vitalsStatus[total_no_of_parameters] = { vitalsIsOk(alert ,vitalNames[0],bpm, bpmlimits[0],bpmlimits[1]),
+							     vitalsIsOk(alert,vitalNames[1],spo2,spo2min,INT_MAX),
+		     					     vitalsIsOk(alert,vitalNames[2],respRate, resplimits[0],resplimits[1]) };
+
+		for (int i = 0; i < total_no_of_parameters; i++) {
+			if (vitalsStatus[i] == 1) {
+				countOfTrueValues++;
+			}
+		}
+		return (countOfTrueValues == total_no_of_parameters);
+	}
+};
+
 
 int main() {
-	assert(vitalsAreOk(80, 95, 60) == true);
-	assert(vitalsAreOk(60, 90, 40) == false);
-	assert(vitalsIsOk("BPM",60, 70, 150) == false);
-	assert(vitalsIsOk("SPO2",200, 90, 150) == false);
-	assert(vitalsIsOk("RESPIRATORY",85, 70, 150) == true);
+	Alert* alert;
+	AlertWithSms alertSms;
+	AlertWithAlarm alertAlarm;
+	VitalsCheck checkVitals;
+	checkVitals.vitalsAreOk(&alertSms,80, 95, 60);
+	checkVitals.vitalsAreOk(&alertAlarm,60, 90, 40);
 }
